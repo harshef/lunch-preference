a292b96dc6bc9a3e0e565255841e69b60f49ed72
harshef <maahi.puri@gmail.com>
2024-11-25 12:12:19 +0530

    Update lunch_preferences_app.py

--- lunch_preferences_app.py
+++ lunch_preferences_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from supabase import create_client
 import os
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 if "delete_clicked" not in st.session_state:
